376fd32e723f8f0c3ea27e0ea9d67cecc30500e4
yuvalro <yuvalro@checkpoint.com>
2026-06-19 18:12:37 +0300

    Updated config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // We don't have route splitting so we increase the chunk size limit to avoid warnings in the console.
+    chunkSizeWarningLimit: 800,
+  },
 })
